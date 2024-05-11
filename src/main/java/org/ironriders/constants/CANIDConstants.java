010f82d5c8e81788eb2769c690d678efe914fa96
a-kraken <83683166+a-kraken@users.noreply.github.com>
2024-05-10 18:12:52 -0700

    Updated vendordeps

---
+++ src/main/java/org/ironriders/constants/CANIDConstants.java
@@ -0,0 +1,17 @@
+package org.ironriders.constants;
+
+public class CANIDConstants {
+    public static final int SWERVE_FR_STEER_CAN_ID = 0;
+    public static final int SWERVE_FR_DRIVE_CAN_ID = 0;
+
+    public static final int SWERVE_FL_STEER_CAN_ID = 0;
+    public static final int SWERVE_FL_DRIVE_CAN_ID = 0;
+
+    public static final int SWERVE_BR_STEER_CAN_ID = 0;
+    public static final int SWERVE_BR_DRIVE_CAN_ID = 0;
+
+    public static final int SWERVE_BL_STEER_CAN_ID = 0;
+    public static final int SWERVE_BL_DRIVE_CAN_ID = 0;
+
+    public static final int PIGEON_CAN_ID = 9;
+}
